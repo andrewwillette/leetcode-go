f14f15ac6e5d210b99bf008d6695595bbddff30d
andrewwillette <willette.andrew1846@gmail.com>
2023-04-16 10:31:00 -0500

    master: validatestacksequences complete

---
+++ validatestacksequences/validatestacksequences_test.go
@@ -0,0 +1,56 @@
+package validatestacksequences
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type stack []int
+
+func (s stack) Push(v int) stack {
+	return append(s, v)
+}
+
+func (s stack) Pop() (stack, int) {
+	l := len(s)
+	if l == 0 {
+		return *new(stack), 0
+	}
+	return s[:l-1], s[l-1]
+}
+
+func validateStackSequences(pushed []int, popped []int) bool {
+	s := *new(stack)
+	var i, j int
+	for i < len(pushed) {
+		s = s.Push(pushed[i])
+		for j < len(popped) && s[len(s)-1] == popped[j] {
+			s, _ = s.Pop()
+			j++
+		}
+		i++
+	}
+	return len(s) == 0
+}
+
+func TestValidateStackSequences(t *testing.T) {
+	var cases = []struct {
+		pushed   []int
+		popped   []int
+		expected bool
+	}{
+		{
+			pushed:   []int{1, 2, 3, 4, 5},
+			popped:   []int{4, 5, 3, 2, 1},
+			expected: true,
+		},
+	}
+	for _, c := range cases {
+		t.Run(fmt.Sprintf(""), func(t *testing.T) {
+			result := validateStackSequences(c.pushed, c.popped)
+			require.Equal(t, c.expected, result)
+		})
+	}
+}
