69c6e46c8e5ba4872af7b37cb067851bded8fa59
andrewwillette <willette.andrew1846@gmail.com>
2023-07-09 21:57:15 -0500

    master: solved climbStairs

--- climbstairs/climbstairs_test.go
+++ climbstairs/climbstairs_test.go
@@ -2,5 +2,22 @@ package climbstairs
 
 // https://leetcode.com/problems/climbing-stairs/
 func climbStairs(n int) int {
-	return 0
+	switch n {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	case 2:
+		return 2
+	default:
+		oneStepBefore := 2
+		twoStepsBefore := 1
+		allWays := 0
+		for i := 2; i < n; i++ {
+			allWays = oneStepBefore + twoStepsBefore
+			twoStepsBefore = oneStepBefore
+			oneStepBefore = allWays
+		}
+		return allWays
+	}
 }
