b53c735968d0ddfabc83a3a50711b1531aa3cb11
andrewwillette <willette.andrew1846@gmail.com>
2025-02-14 09:56:57 -0600

    adding testcase

--- productoflastk/productoflastk_test.go
+++ productoflastk/productoflastk_test.go
@@ -1,5 +1,11 @@
 package productoflastk
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
 // https://leetcode.com/problems/product-of-the-last-k-numbers
 type ProductOfNumbers struct {
 	stream []int
@@ -20,4 +26,34 @@ func (this *ProductOfNumbers) GetProduct(k int) int {
 		res = res * toProduct[i]
 	}
 	return res
+}
+
+func TestProductOfNumbers(t *testing.T) {
+	var cases = []struct {
+		actions  []string
+		values   []int
+		expected []int
+	}{
+		{
+			actions:  []string{"ProductOfNumbers", "add", "add", "add", "add", "add", "getProduct", "getProduct", "getProduct", "add", "getProduct"},
+			values:   []int{0, 3, 0, 2, 5, 4, 2, 3, 4, 8, 2},
+			expected: []int{-1, 3, 0, 0, 0, 0, 20, 40, 0, 0, 32},
+		},
+	}
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
+			var obj ProductOfNumbers
+			for i, action := range c.actions {
+				switch action {
+				case "ProductOfNumbers":
+					obj = Constructor()
+				case "add":
+					obj.Add(c.values[i])
+				case "getProduct":
+					result := obj.GetProduct(c.values[i])
+					require.Equal(t, c.expected[i], result)
+				}
+			}
+		})
+	}
 }
